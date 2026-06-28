d7afc3aa3a68ed231f2a610b0012877c5ff66775
student-IETLucknow16 <electronicsengineer2028@gmail.com>
2026-06-28 23:45:29 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:5173'],
+  origin: ['http://localhost:3000', 'https://task-tracker-frontend-4ajr.onrender.com'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
